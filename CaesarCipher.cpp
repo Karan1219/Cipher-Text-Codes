d56db8f0ba01dbe4bc0e2c3f23423bbedb539178
karan <singhkaranpreet498@gmail.com>
2020-09-05 14:14:29 +0530

    Cipher Code

---
+++ CaesarCipher.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include<vector>
+using namespace std;
+
+int main()
+{
+    string s;
+    cin>>s;
+    cout<<"Karanpreet Singh (045)"<<endl;
+    cout<<"\nPlain Text :\n"<<s<<endl;
+    string enc="",dec="";
+    int k,ki;
+    cin>>k;
+    cout<<"Key : "<<k<<endl<<endl;
+    vector<int> vect;
+    for(int i=0;i<s.length();i++)
+    {
+       int val=s[i]-'a';
+       vect.push_back(val);
+    }
+    for(int i=0;i<vect.size();i++)
+    {
+        vect[i]=(vect[i]+k)%26;
+    }
+    for(int i=0;i<vect.size();i++)
+    {
+        char c=vect[i]+'A';
+        enc=enc+c;
+       
+    }
+    cout<<"Encrypted/Cipher Text: \n"<<enc;
+    cout<<"\n \nEncrypted using Caesar Cipher ";
+    cout<<"\n\nNow Decrypting :\n"<<enc<<endl;
+    
+    for(int i=0;i<vect.size();i++)
+    {
+        if(vect[i]-k<0)
+        {
+            vect[i]=26+(vect[i]-k);
+        }
+        else
+           vect[i]=(vect[i]-k)%26;
+    }
+    for(int i=0;i<vect.size();i++)
+    {
+        
+        char c=vect[i]+'a';
+        dec=dec+c;
+       
+    }
+   cout<<"Plain Text comes out to be: \n"<<dec<<endl;
+   
+   return 0;
+}
---
+++ MultiplicativeCipher.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include<vector>
+using namespace std;
+
+int main()
+{
+    string s;
+    cin>>s;
+    cout<<"Karanpreet Singh (045)"<<endl;
+    cout<<"\nPlain Text :\n"<<s<<endl;
+    string enc="",dec="";
+    int k,ki;
+    cin>>k;
+    cout<<"Key : "<<k<<endl<<endl;
+    vector<int> vect;
+    for(int i=0;i<s.length();i++)
+    {
+       int val=s[i]-'a';
+       vect.push_back(val);
+    }
+    for(int i=0;i<vect.size();i++)
+    {
+        vect[i]=(vect[i]*k)%26;
+    }
+    for(int i=0;i<vect.size();i++)
+    {
+        char c=vect[i]+'A';
+        enc=enc+c;
+       
+    }
+    cout<<"Encrypted/Cipher Text: \n"<<enc;
+    cout<<"\n \nEncrypted using Multiplicative Cipher ";
+    cout<<"\n\nNow Decrypting :\n"<<enc<<endl;
+    
+    
+    for(int i=1;i<999999;i++)
+    {
+        if((k*i)%26==1)
+        {
+            ki=i;
+            break;
+        }
+    }
+    cout<<"Inverse Key : "<<ki<<endl<<endl;
+    for(int i=0;i<vect.size();i++)
+    {
+        vect[i]=(vect[i]*ki)%26;
+    }
+    for(int i=0;i<vect.size();i++)
+    {
+        char c=vect[i]+'a';
+        dec=dec+c;
+       
+    }
+   cout<<"Plain Text comes out to be: \n"<<dec<<endl;
+   
+   return 0;
+}
